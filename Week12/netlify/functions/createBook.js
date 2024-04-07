c7e827bac41e3d2c51e5eced8ae79148467db40d
Prab11 <157606475+Prab11@users.noreply.github.com>
2024-04-07 09:30:41 +0500

    Sixth Commit

---
+++ Week12/netlify/functions/createBook.js
@@ -0,0 +1,38 @@
+const mysql = require('mysql2/promise');
+
+exports.handler = async (event, context) => {
+  // if (!context.clientContext || !context.clientContext.user) {
+  //   return {
+  //   statusCode: 401,
+  //   body: JSON.stringify({ error: 'You must be logged in.' }),
+  //   };
+  //   }
+    
+  //   const user = context.clientContext.user;
+  
+  //   console.log('Authenticated user:', user);
+  try {
+    const { title, author, isbn, published_year, genre } = JSON.parse(event.body);
+ 
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    });
+
+    const sql = 'INSERT INTO pramal1 (title, author, isbn, published_year, genre) VALUES (?, ?, ?, ?, ?)';
+    await connection.execute(sql, [title, author, isbn, published_year, genre]);
+    await connection.end();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Book created successfully" }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to create book." }),
+    };
+  }
+};
---
+++ Week12/netlify/functions/readBook.js
@@ -0,0 +1,44 @@
+const mysql = require('mysql2/promise');
+
+exports.handler = async (event, context) => {
+  // if (!context.clientContext || !context.clientContext.user) {
+  //   return {
+  //   statusCode: 401,
+  //   body: JSON.stringify({ error: 'You must be logged in.' }),
+  //   };
+  //   }
+    
+  //   const user = context.clientContext.user;
+  
+  //   console.log('Authenticated user:', user);
+  try {
+    const { id } = event.queryStringParameters;
+
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    });
+
+    const [book] = await connection.query('SELECT * FROM pramal1 WHERE id = ?', [id]);
+    await connection.end();
+
+    if (book.length === 0) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "Book not found" }),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(book[0]),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to get book." }),
+    };
+  }
+};
